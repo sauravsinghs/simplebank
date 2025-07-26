a0a8ec84e83c89569110888880071ea0b3b58a68
Saurav Singh <147067494+sauravsinghs@users.noreply.github.com>
2025-07-26 05:52:05 +0530

    update fix 2 (#14)
    * load env
    
    * permission fix
    
    * update github-ci to deploy to amazon EKS
    
    * update fix
    
    * update fix 2

--- util/config.go
+++ util/config.go
@@ -7,10 +7,11 @@ import (
 )
 
 // Config stores all configuration of the application.
-// The values are read by viper from a config file or environment variable.
+// The values are read by viper from a config file or environment variables.
 type Config struct {
 	Environment          string        `mapstructure:"ENVIRONMENT"`
 	AllowedOrigins       []string      `mapstructure:"ALLOWED_ORIGINS"`
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
 	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
